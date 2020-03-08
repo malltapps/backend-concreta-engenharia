48a2f36d8a9bdbdd03bcdb0b19744674c2a93955
Mallt Apps <58448039+malltapps@users.noreply.github.com>
2020-03-08 01:14:19 -0300

    Add files via upload

--- dist/app.js
+++ dist/app.js
@@ -1,21 +1,30 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }require('dotenv/config');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
+require("dotenv/config");
 const express = require("express");
-var _express = require('express'); var _express2 = _interopRequireDefault(_express);
-var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
-var _youch = require('youch'); var _youch2 = _interopRequireDefault(_youch);
-var _routes = require('./routes'); var _routes2 = _interopRequireDefault(_routes);
-require('./database');
+var _express = require("express");
+var _express2 = _interopRequireDefault(_express);
+var _cors = require("cors");
+var _cors2 = _interopRequireDefault(_cors);
+var _youch = require("youch");
+var _youch2 = _interopRequireDefault(_youch);
+var _routes = require("./routes");
+var _routes2 = _interopRequireDefault(_routes);
+require("./database");
 
 class App {
   constructor() {
-    this.server = _express2.default.call(void 0, );
+    this.server = _express2.default.call(void 0);
     this.middlewares();
     this.routes();
     this.exceptionHandler();
   }
 
   middlewares() {
-    this.server.use(_cors2.default.call(void 0, ));
+    this.server.use(_cors2.default.call(void 0));
     this.server.use(_express2.default.json());
   }
 
@@ -24,10 +33,10 @@ class App {
   }
   exceptionHandler() {
     this.server.use(async (err, req, res, next) => {
-      if (process.env.NODE_ENV === "development") {
+      /* if (process.env.NODE_ENV === "development") {
         const errors = await new (0, _youch2.default)(err, req).toJSON();
         return res.status(500).json(errors);
-      }
+      }*/
       return res.status(500).json({
         error: "Internal Server Error",
       });
@@ -35,4 +44,4 @@ class App {
   }
 }
 
-exports. default = new App().server;
+exports.default = new App().server;
